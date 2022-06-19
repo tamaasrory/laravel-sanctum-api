d9257529cbf8b4af4b4e7349d47d6ad0131584a8
tama <tama1234>
2022-06-19 22:36:23 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/tests', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+    $token = $user->createToken('myapp')->plainTextToken;
+    return [$user, $token];
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
